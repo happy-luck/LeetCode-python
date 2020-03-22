85549bda6b65c1197f77d95bfad2886483290d3b
happy-luck <18813129242@163.com>
2020-03-22 17:03:05 +0800

    单向链表

---
+++ 203.移除链表元素.py
@@ -0,0 +1,17 @@
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def removeElements(self, head: ListNode, val: int) -> ListNode:
+        sen = ListNode(0)
+        sen.next = head
+        pre,cur = sen,head
+        while cur:
+            if cur.val == val:
+                pre.next = cur.next
+            else:
+                pre = cur
+            cur = cur.next
+        return sen.next
