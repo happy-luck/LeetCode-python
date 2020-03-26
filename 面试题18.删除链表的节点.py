bf2ec34f7c70e6069a95d155b2c6f53d2feda272
happy-luck <18813129242@163.com>
2020-03-26 08:52:52 +0800

    单向链表,237升级版

--- 面试题18.删除链表的节点.py
+++ 面试题18.删除链表的节点.py
@@ -1,3 +1,4 @@
+方法一：
 class ListNode:
     def __init__(self, x):
         self.val = x
@@ -13,3 +14,15 @@ class Solution:
                 pre.next = pre.next.next
                 return sen.next
             pre = pre.next
+方法二：
+class Solution:
+    def deleteNode(self, head: ListNode, val: int) -> ListNode:
+        if head.val == val: return head.next
+        pre, cur = head, head.next
+        while cur and cur.val != val:
+            pre, cur = cur, cur.next
+        pre.next = cur.next
+        return head
+
+时间复杂度 O(N) ： N 为链表长度，删除操作平均需循环 N/2 次，最差 N 次。
+空间复杂度 O(1) ： cur, pre 占用常数大小额外空间。
