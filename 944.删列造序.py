8e341e0f17c19f90a263dc714fa0aac62d401248
happy-luck <18813129242@163.com>
2020-04-18 09:51:55 +0800

    贪心

--- 944.删列造序.py
+++ 944.删列造序.py
@@ -19,7 +19,7 @@ class Solution(object):
     def minDeletionSize(self, A):
         ans = 0
         for col in zip(*A):
-            if any(col[i] > col[i+1] for i in xrange(len(col) - 1)):
+            if any(col[i] > col[i+1] for i in range(len(col) - 1)):
                 ans += 1
         return ans
 
